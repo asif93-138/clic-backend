921fe02921bfafe0619f703fd80917502616b7ce
asif93-138 <asif93@student.sust.edu>
2025-07-21 19:17:27 +0600

    working on email verrification controller..

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -6,6 +6,8 @@ import CallHistory from '../models/callHistory';
 import Matched from '../models/matched';
 import WaitingRoom from '../models/waitingRoom';
 import FailedClic from '../models/failedClic';
+import User from '../models/user.model';
+import DateFeedback from '../models/dateFeedback';
 
 const email = "admin@email.com";
 const password = "admin";
@@ -28,10 +30,9 @@ export async function adminDataEvent(req: Request, res: Response) {
     const events = await Event.find({ date_time: { $lt: new Date().toISOString() } }, "") as Array<{ _id: any, title: string, date_time: any }>;
     for (const event of events) {
         const approvedList = await eventUser.find({event_id: event._id, status: "approved"});
-        const attendees = await WaitingRoom.find({event_id: event._id, status: "inactive"});
-        const attendeesM = await WaitingRoom.find({event_id: event._id, status: "inactive", gender: "M"});
-        const attendeesF = await WaitingRoom.find({event_id: event._id, status: "inactive", gender: "F"});
-        
+        const attendees = await WaitingRoom.find({event_id: event._id});
+        const attendeesM = await WaitingRoom.find({event_id: event._id, gender: "M"});
+        const attendeesF = await WaitingRoom.find({event_id: event._id, gender: "F"});
         const callList = await CallHistory.find({event_id: event._id});
         const leftEarly = await CallHistory.find({event_id: event._id, left_early: true});
         const matchList = await Matched.find({event_id: event._id});
@@ -59,6 +60,45 @@ matchList.forEach(x => {
 }
 
 export async function adminEventDetails(req: Request, res: Response) {
-    console.log(req.params.id);
-    res.send("Ok");
+    const attendees = await WaitingRoom.find({event_id: req.params.id}, "user_id");
+    const arr: string[] = [];
+    attendees.forEach(x => arr.push(x.user_id));
+    const users = await User.find({ _id : { $in: arr } }, "userName imgURL gender city") as Array<{ _id: string, userName: string, imgURL: string, gender: string, city: string }>;
+    const callList = await CallHistory.find({event_id: req.params.id});
+    const feedBacks = await DateFeedback.find({event_id: req.params.id});
+    const failedClics = await FailedClic.find({event_id: req.params.id});
+    const matchList = await Matched.find({event_id: req.params.id});
+    const userData:any = {};
+    const userArr:any = [];
+    arr.forEach((x: any) => {
+        for (const y of users) {
+            if (y._id.toString() == x) {
+                userData[x] = y;
+                userArr.push(y);
+                break;
+            }
+        }
+    });
+    const callData: any = [];
+    callList.forEach(call => {
+        const data:any = call.toObject();
+        if (call.dateRoomDocId == matchList[0]?.dateRoomDocId) {
+            data.matchCount = matchList[0].count;
+            matchList.shift();
+        }
+        if (call.dateRoomDocId == failedClics[0]?.dateRoomDocId) {
+            data.failedClic = failedClics[0].requested;
+            failedClics.shift();
+        }
+        if (call.dateRoomDocId == feedBacks[0]?.dateRoomDocId) {
+            data.feedBack = [feedBacks[0]];
+            feedBacks.shift();
+            if (call.dateRoomDocId == feedBacks[0]?.dateRoomDocId) {
+                data.feedBack = [...data.feedBack, feedBacks[0]];
+                feedBacks.shift();
+            }
+        }
+        callData.push(data);
+    });
+    res.json({userData, userArr, callData});
 }
