8f98d48313fc4523636c5b38f7b4d994b92b93f7
asif93-138 <asif93@student.sust.edu>
2025-09-18 23:54:33 +0600

    datefeedback schema updated

--- src/controllers/feedbackCollection.ts
+++ src/controllers/feedbackCollection.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import DateFeedback from "../models/dateFeedback";
 
 export default async function collectFeedback(req: Request, res: Response): Promise<void> {
+    console.log(req.body);
     const result = await DateFeedback.create(req.body);
     res.json(result);
 }
--- src/models/dateFeedback.ts
+++ src/models/dateFeedback.ts
@@ -7,7 +7,7 @@ const DateFeedbackSchema = new mongoose.Schema(
     other_user_id: { type: String, required: true },
     dateRoomDocId: { type: String, required: true },
     swipeRight: { type: String, required: true },
-    wanterToTalkLonger: { type: String, required: true },
+    wantedToTalkLonger: { type: String, required: true },
     rating: {type: Number, required: true},
   },
   { timestamps: true }
